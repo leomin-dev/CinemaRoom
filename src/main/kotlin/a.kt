76cd4cbd3dc910502ce7794395e5e6b7f4412b9a
leomin-dev <study31203@gmail.com>
2025-06-27 13:59:31 +0900

    Update a.kt

--- src/main/kotlin/a.kt
+++ src/main/kotlin/a.kt
@@ -2,13 +2,5 @@ package org.example
 
 
 fun main() {
-    val dorm = readln()
-
-    println(when(dorm) {
-        "gryffindor" -> "bravery"
-        "hufflepuff" -> "loyalty"
-        "slytherin" -> "cunning"
-        "ravenclaw" -> "intellect"
-        else -> "not a valid house"
-    })
+    
 }
